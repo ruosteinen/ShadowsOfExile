b3ce5d30452e73313ce9386a147fc0796977e517
Nikolai Telin <telinnikolai@gmail.com>
2024-02-18 02:28:52 +0000

    added Quake3-like motion mechanics and a Singleton pattern for the pause menu

---
+++ Assets/Script/Menu/PauseMenuSingleton.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuSingleton : MonoBehaviour
+{
+    public static PauseMenuSingleton Instance { get; private set; }
+
+    public GameObject pauseMenuUI;
+    private bool _isPaused;  //false by default
+
+    public bool IsPaused
+    {
+        get { return _isPaused; }
+        private set { _isPaused = value; }
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+            //DontDestroyOnLoad(gameObject); // If we need to save the pause menu between scenes (no fucking need)
+        else
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //Stop game time
+        IsPaused = true;
+        
+        //Unlocking the cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //Resuming game time
+        IsPaused = false;
+        
+        //Cursor lock
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f; //Resuming game time
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Script/PlayerQ3LikeController.cs
@@ -0,0 +1,361 @@
+using UnityEngine;
+
+// Contains the command the user wishes upon the character
+struct Directions
+{
+    public float ToForward;
+    public float ToRight;
+}
+
+public class PlayerQ3LikeController : MonoBehaviour
+{
+    public Transform playerView; // Camera
+    
+    [SerializeField]private float playerViewYOffset = 0.6f; // The height at which the camera is bound to
+    [SerializeField]private float xMouseSensitivity = 30.0f;
+    [SerializeField]private float yMouseSensitivity = 30.0f;
+
+    
+    [SerializeField]private float gravity = 20.0f;
+    [SerializeField]private float groundFriction = 6;
+    [SerializeField]private float speedOnGround = 7.0f;
+    [SerializeField]private float runAcceleration = 14.0f;
+    [SerializeField]private float runDeacceleration = 10.0f;
+    [SerializeField]private float airAcceleration = 2.0f;
+    [SerializeField]private float airDecceleration = 2.0f;          //Deacceleration experienced when oposite strafing
+    [SerializeField]private float airControl = 0.3f;               //How precise air control is
+    [SerializeField]private float sideStrafeAcceleration = 50.0f;  //How fast acceleration occurs to get up to sideStrafeSpeed when
+    [SerializeField]private float sideStrafeSpeed = 1.0f;         //What the max speed to generate when side strafing
+    [SerializeField]private float jumpSpeed = 8.0f;              //The speed at which the character's up axis gains when hitting jump
+    [SerializeField]private bool holdJumpToBhop;                 //Enables bhop when the jump button is pressed
+    
+    public GUIStyle style;
+
+    //FPS
+    public float fpsDisplayRate = 4.0f; // 4 updates per sec
+
+    private int _frameCount; //0 by default
+    private float _dt;      //0.0f by default
+    private float _fps;    //0.0f by default
+
+    private CharacterController _controller;
+
+    // Camera rotations
+    private float _rotX; //0.0f by default
+    private float _rotY; //0.0f by default
+
+    private Vector3 _moveDirectionNorm = Vector3.zero;
+    private Vector3 _playerVelocity = Vector3.zero;
+    private float _playerTopVelocity;//0.0f by default
+
+    // Q3: players can queue the next jump just before he hits the ground
+    private bool _wishJump; //false by default
+
+    // Used to display real time fricton values
+    private float _playerFriction;//0.0f by default
+
+    // Player commands, stores wish commands that the player asks for (Forward, Right)
+    private Directions _dirs;
+    
+    
+    private void Start()
+    {
+        // Hide the cursor
+     
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        if (playerView == null)
+        {
+            Camera mainCamera = Camera.main;
+            if (mainCamera != null)
+                playerView = mainCamera.gameObject.transform;
+        }
+
+        // Put the camera inside the capsule collider
+        Vector3 currentPosition = transform.position;
+        
+        playerView.position = new Vector3(currentPosition.x, 
+            currentPosition.y + playerViewYOffset,
+            currentPosition.z);
+        
+        _controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update()
+    {
+        if (PauseMenuSingleton.Instance.IsPaused)
+        {
+            
+            return;
+        }
+        
+        //FPS calculation
+        _frameCount++;
+        _dt += Time.deltaTime;
+        if (_dt > 1.0 / fpsDisplayRate)
+        {
+            _fps = Mathf.Round(_frameCount / _dt);
+            _frameCount = 0;
+            _dt -= 1.0f / fpsDisplayRate;
+        }
+        
+        //Cursor locking
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            if (Input.GetButtonDown("Fire1"))
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+        
+        _rotX -= Input.GetAxisRaw("Mouse Y") * xMouseSensitivity * 0.02f;
+        _rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * 0.02f;
+
+        // Clamp the X rotation
+        if(_rotX < -90)
+            _rotX = -90;
+        else if(_rotX > 90)
+            _rotX = 90;
+
+        transform.rotation = Quaternion.Euler(0, _rotY, 0); // Rotates body
+        playerView.rotation = Quaternion.Euler(_rotX, _rotY, 0); // Rotates camera
+
+        
+        QueueJump();
+        if(_controller.isGrounded)
+            GroundMove();
+        else if(!_controller.isGrounded)
+            AirMove();
+
+        // Move the controller
+        _controller.Move(_playerVelocity * Time.deltaTime);
+
+        //Top velocity calc
+        Vector3 udp = _playerVelocity;
+        udp.y = 0.0f;
+        if(udp.magnitude > _playerTopVelocity)
+            _playerTopVelocity = udp.magnitude;
+
+        //Need to move the camera after the player has been moved because otherwise the camera will clip the player if going fast enough and will always be 1 frame behind.
+        // Set the camera's position to the transform
+        
+        /*playerView.position = new Vector3(
+            transform.position.x,
+            transform.position.y + playerViewYOffset,
+            transform.position.z);*/
+        
+        Vector3 currentPosition = transform.position;
+        playerView.position = new Vector3(currentPosition.x, 
+            currentPosition.y + playerViewYOffset,
+            currentPosition.z);
+    }
+    
+    
+    private void SetMovementDir()
+    {
+        _dirs.ToForward = Input.GetAxisRaw("Vertical");
+        _dirs.ToRight = Input.GetAxisRaw("Horizontal");
+    }
+    
+    
+     //Queues the next jump just like in Q3
+     private void QueueJump()
+    {
+        if(holdJumpToBhop)
+        {
+            _wishJump = Input.GetButton("Jump");
+            return;
+        }
+
+        if(Input.GetButtonDown("Jump") && !_wishJump)
+            _wishJump = true;
+        if(Input.GetButtonUp("Jump"))
+            _wishJump = false;
+    }
+
+   //Execs when the player is in the air
+    private void AirMove()
+    {
+        Vector3 wishdir;
+        //float wishvel = airAcceleration;
+        float accel;
+        
+        SetMovementDir();
+
+        wishdir =  new Vector3(_dirs.ToRight, 0, _dirs.ToForward);
+        wishdir = transform.TransformDirection(wishdir);
+
+        float wishspeed = wishdir.magnitude;
+        wishspeed *= speedOnGround;
+
+        wishdir.Normalize();
+        _moveDirectionNorm = wishdir;
+        
+        
+        float wishspeed2 = wishspeed;
+        if (Vector3.Dot(_playerVelocity, wishdir) < 0)
+            accel = airDecceleration;
+        else
+            accel = airAcceleration;
+        
+        
+        // If the player is ONLY strafing left or right
+        if(_dirs.ToForward == 0 && _dirs.ToRight != 0)
+        {
+            if(wishspeed > sideStrafeSpeed)
+                wishspeed = sideStrafeSpeed;
+            accel = sideStrafeAcceleration;
+        }
+
+        Accelerate(wishdir, wishspeed, accel);
+        if(airControl > 0)
+            AirControl(wishdir, wishspeed2);
+       
+
+        // Apply gravity
+        _playerVelocity.y -= gravity * Time.deltaTime;
+    }
+
+    /*
+     Air control occurs when the player is in the air, it allows
+     players to move side to side much faster rather than being
+      'sluggish' when it comes to cornering.
+     */
+    private void AirControl(Vector3 wishdir, float wishspeed)
+    {
+        float zSpeed;
+        float speed;
+        float dotProd;
+        float k;
+
+        // Can't control movement if not moving forward or backward
+        if(Mathf.Abs(_dirs.ToForward) < 0.001 || Mathf.Abs(wishspeed) < 0.001)
+            return;
+        
+        zSpeed = _playerVelocity.y;
+        _playerVelocity.y = 0;
+        
+        
+        speed = _playerVelocity.magnitude;
+        _playerVelocity.Normalize();
+
+        dotProd = Vector3.Dot(_playerVelocity, wishdir);
+        k = 32;
+        k *= airControl * dotProd * dotProd * Time.deltaTime;
+
+        // Change direction while slowing down
+        if (dotProd > 0)
+        {
+            _playerVelocity.x = _playerVelocity.x * speed + wishdir.x * k;
+            _playerVelocity.y = _playerVelocity.y * speed + wishdir.y * k;
+            _playerVelocity.z = _playerVelocity.z * speed + wishdir.z * k;
+
+            _playerVelocity.Normalize();
+            _moveDirectionNorm = _playerVelocity;
+        }
+
+        _playerVelocity.x *= speed;
+        _playerVelocity.y = zSpeed;
+        _playerVelocity.z *= speed;
+    }
+    
+    
+    //Called every frame when the engine detects that the player is on the ground
+    private void GroundMove()
+    {
+        Vector3 wishdir;
+
+        // Do not apply friction if the player is queueing up the next jump
+        if (!_wishJump)
+            ApplyFriction(1.0f);
+        else
+            ApplyFriction(0);
+
+        SetMovementDir();
+
+        wishdir = new Vector3(_dirs.ToRight, 0, _dirs.ToForward);
+        wishdir = transform.TransformDirection(wishdir);
+        wishdir.Normalize();
+        _moveDirectionNorm = wishdir;
+
+        float wishspeed = wishdir.magnitude;
+        wishspeed *= speedOnGround;
+
+        Accelerate(wishdir, wishspeed, runAcceleration);
+
+        // Reset the gravity velocity
+        _playerVelocity.y = -gravity * Time.deltaTime;
+
+        if(_wishJump)
+        {
+            _playerVelocity.y = jumpSpeed;
+            _wishJump = false;
+        }
+    }
+
+    
+     //Applies friction to the player everywhere
+    private void ApplyFriction(float t)
+    {
+        /*Any changes made to vec after this assignment
+         will not affect the original _playerVelocity vector, 
+         since vec is a separate copy of vec*/
+        
+        Vector3 vec = _playerVelocity;
+        float speed;
+        float newSpeed;
+        float control;
+        float drop;
+
+        vec.y = 0.0f;
+        speed = vec.magnitude;
+        drop = 0.0f;
+
+        //Only if the player is on the ground 
+        if(_controller.isGrounded)
+        {
+            control = speed < runDeacceleration ? runDeacceleration : speed;
+            drop = control * groundFriction * Time.deltaTime * t;
+        }
+
+        newSpeed = speed - drop;
+        _playerFriction = newSpeed;
+        
+        if(newSpeed < 0)
+            newSpeed = 0;
+        if(speed > 0)
+            newSpeed /= speed;
+
+        _playerVelocity.x *= newSpeed;
+        _playerVelocity.z *= newSpeed;
+    }
+
+    private void Accelerate(Vector3 wishdir, float wishspeed, float accel)
+    {
+        float addSpeed;
+        float accelSpeed;
+        float currentSpeed;
+
+        currentSpeed = Vector3.Dot(_playerVelocity, wishdir);
+        addSpeed = wishspeed - currentSpeed;
+        
+        if(addSpeed <= 0)
+            return;
+        
+        accelSpeed = accel * Time.deltaTime * wishspeed;
+        
+        if(accelSpeed > addSpeed)
+            accelSpeed = addSpeed;
+
+        _playerVelocity.x += accelSpeed * wishdir.x;
+        _playerVelocity.z += accelSpeed * wishdir.z;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 400, 100), "FPS: " + _fps, style);
+        var ups = _controller.velocity;
+        ups.y = 0;
+        GUI.Label(new Rect(0, 15, 400, 100), "Speed: " + Mathf.Round(ups.magnitude * 100) / 100, style);
+        GUI.Label(new Rect(0, 30, 400, 100), "Top Speed: " + Mathf.Round(_playerTopVelocity * 100) / 100 , style);
+    }
+}
