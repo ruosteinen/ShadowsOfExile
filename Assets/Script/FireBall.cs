bd9ece0a7fc335677cfa4b121c552a10ca8f6081
Nikolai Telin <telinnikolai@gmail.com>
2024-04-10 14:04:25 +0100

    started working on the ground fire

--- Assets/Script/FireBall.cs
+++ Assets/Script/FireBall.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class FireBall : MonoBehaviour
 {
     public float maxDistance;
+    public float affectedRadius = 5f;
+    public LayerMask groundLayer;
     private Vector3 throwPosition;
 
     public void Initialize(Vector3 initialThrowPosition) => throwPosition = initialThrowPosition;
@@ -19,6 +21,15 @@ public class FireBall : MonoBehaviour
         {
             Flammable flammable = collision.gameObject.GetComponent<Flammable>();
             if (flammable != null && !flammable.isOnFire) flammable.Ignite();
+        }
+        else if (groundLayer == (groundLayer | (1 << collision.gameObject.layer)))
+        {
+            Collider[] colliders = Physics.OverlapSphere(collision.contacts[0].point, affectedRadius);
+            foreach (Collider col in colliders)
+            {
+                ParticleSystem ps = col.GetComponent<ParticleSystem>();
+                if (ps != null) ps.Play();
+            }
         }
         Destroy(gameObject);
     }
