242474a12110c50103a63ea608a07ec7a1503ddf
Nikolai Telin <telinnikolai@gmail.com>
2024-04-14 22:28:36 +0100

    added ground smoke

--- Assets/GroundFire.cs
+++ Assets/GroundFire.cs
@@ -4,65 +4,91 @@ using System.Collections.Generic;
 
 public class GroundFire : MonoBehaviour
 {
-    public GameObject particleSystemPrefab;
+    public GameObject fireParticleSystemPrefab;
+    public GameObject smokeParticleSystemPrefab;
     public GameObject particleModelPrefab;
     public float maxScale = 4f;
     public float scaleSpeed = 0.1f;
     public float particleSystemDuration = 8f;
 
-    private List<ParticleSystem> activeParticleSystems = new ();
+    private List<ParticleSystem> activeParticleSystems = new List<ParticleSystem>();
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("FireBall"))
         {
             Vector3 collisionPoint = collision.contacts[0].point;
-            GameObject particleSystemObject = Instantiate(particleSystemPrefab, collisionPoint, Quaternion.identity);
-            ParticleSystem particleSystemComponent = particleSystemObject.GetComponent<ParticleSystem>();
-
-            ParticleSystem.ShapeModule shapeModule = particleSystemComponent.shape;
-            shapeModule.shapeType = ParticleSystemShapeType.Mesh;
-            shapeModule.mesh = particleModelPrefab.GetComponent<MeshFilter>().sharedMesh;
-
-            particleSystemObject.transform.localScale = new Vector3(1, 1, 1);
-            activeParticleSystems.Add(particleSystemComponent);
-            StartCoroutine(ScaleOverTimeAndDestroy(particleSystemObject, maxScale, scaleSpeed, particleSystemDuration));
+            StartFireAndSmokeSystems(collisionPoint);
         }
-
-        if (collision.gameObject.CompareTag("WaterBall"))
+        else if (collision.gameObject.CompareTag("WaterBall"))
         {
-            ParticleSystem psToRemove = null; // Initialize variable to store particle system to remove from list
-
-            foreach (ParticleSystem ps in activeParticleSystems)
-            {
-                // Calculate the distance between ps and collision point
-                float distance = Vector3.Distance(ps.transform.position, collision.contacts[0].point);
-                if (distance < ps.transform.localScale.x * 2f || distance < ps.transform.localScale.y * 2f || distance < ps.transform.localScale.z * 2f)
-                {
-                    ps.Stop();
-                    psToRemove = ps; // Store reference to the particle system to remove from list
-                }
-            }
-            if (psToRemove != null) activeParticleSystems.Remove(psToRemove); 
+            Vector3 collisionPoint = collision.contacts[0].point;
+            StopParticleSystemAtPoint(collisionPoint);
         }
     }
 
-    private IEnumerator ScaleOverTimeAndDestroy(GameObject particleSystemObject, float maxScale, float scaleSpeed, float duration)
+    private void StartFireAndSmokeSystems(Vector3 collisionPoint)
     {
-        ParticleSystem ps = particleSystemObject.GetComponent<ParticleSystem>();
-        float currentScale = 1.0f;
-        float timer = 0;
+        // Instantiate fire particle system
+        GameObject fireSystemObject = Instantiate(fireParticleSystemPrefab, collisionPoint, Quaternion.identity);
+        ParticleSystem fireSystemComponent = fireSystemObject.GetComponent<ParticleSystem>();
+        SetParticleSystemShape(fireSystemComponent);
+        activeParticleSystems.Add(fireSystemComponent);
 
-        while (currentScale < maxScale)
+        // Instantiate smoke particle system at the same collision point
+        GameObject smokeSystemObject = Instantiate(smokeParticleSystemPrefab, collisionPoint, Quaternion.identity);
+        ParticleSystem smokeSystemComponent = smokeSystemObject.GetComponent<ParticleSystem>();
+        SetParticleSystemShape(smokeSystemComponent);
+        activeParticleSystems.Add(smokeSystemComponent);
+
+        // Start the coroutine for both systems
+        StartCoroutine(ScaleOverTimeAndDestroy(fireSystemObject, maxScale, scaleSpeed, particleSystemDuration));
+        StartCoroutine(ScaleOverTimeAndDestroy(smokeSystemObject, maxScale, scaleSpeed, particleSystemDuration));
+    }
+
+    private void SetParticleSystemShape(ParticleSystem particleSystemComponent)
+    {
+        ParticleSystem.ShapeModule shapeModule = particleSystemComponent.shape;
+        shapeModule.shapeType = ParticleSystemShapeType.Mesh;
+        shapeModule.mesh = particleModelPrefab.GetComponent<MeshFilter>().sharedMesh;
+    }
+
+    private void StopParticleSystemAtPoint(Vector3 collisionPoint)
+    {
+        List<ParticleSystem> systemsToRemove = new List<ParticleSystem>();
+        foreach (ParticleSystem ps in activeParticleSystems)
         {
-            currentScale = Mathf.Lerp(1, maxScale, timer / scaleSpeed);
+            float distance = Vector3.Distance(ps.transform.position, collisionPoint);
+            if (distance < ps.transform.localScale.x * 2f)
+            {
+                ps.Stop();
+                systemsToRemove.Add(ps);
+            }
+        }
+        foreach (ParticleSystem ps in systemsToRemove)
+        {
+            activeParticleSystems.Remove(ps);
+        }
+    }
+
+    private IEnumerator ScaleOverTimeAndDestroy(GameObject particleSystemObject, float targetScale, float speed, float duration)
+    {
+        float timer = 0;
+        Vector3 initialScale = particleSystemObject.transform.localScale;
+        Vector3 targetScaleVector = new Vector3(targetScale, targetScale, targetScale);
+
+        while (particleSystemObject.transform.localScale != targetScaleVector)
+        {
+            float currentScale = Mathf.Lerp(1, targetScale, timer / speed);
             particleSystemObject.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+            if (timer / speed >= 1.0f) break; // Prevent overshooting
             timer += Time.deltaTime;
             yield return null;
         }
 
         yield return new WaitForSeconds(duration);
 
+        ParticleSystem ps = particleSystemObject.GetComponent<ParticleSystem>();
         activeParticleSystems.Remove(ps);
         Destroy(particleSystemObject);
     }
