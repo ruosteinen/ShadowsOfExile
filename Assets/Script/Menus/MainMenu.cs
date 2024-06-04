376f89b8931399c27f7c56d0bcb8c879944e9d39
Nikolai Telin <telinnikolai@gmail.com>
2024-06-04 18:19:24 +0100

    almost ,bro

--- Assets/Script/Menus/MainMenu.cs
+++ Assets/Script/Menus/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Game tutorial");
     }
     
     public void ActivateSettingsMenu()
